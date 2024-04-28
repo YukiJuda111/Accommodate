38b0f5c050cb0933fe50a7f0a148bd8fdc14fdd9
YukiJuda111 <3229971508@qq.com>
2024-04-28 21:26:43 +0800

    加载地域信息

--- web/model/mysql/mysqlFunc.go
+++ web/model/mysql/mysqlFunc.go
@@ -1,12 +1,1 @@
 package model
-
-func RegisterUser(phoneNum string, password string) error {
-
-	user := User{
-		Name:         phoneNum,
-		PasswordHash: password,
-		Mobile:       phoneNum,
-	}
-
-	return GlobalDB.Create(&user).Error
-}
