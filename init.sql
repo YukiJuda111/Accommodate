b75f924f446ad652e0aac5b42360bf0ed3738f07
N0w13re <xuharry2021@163.com>
2024-06-10 16:51:30 +0800

    在init.sql中新建表

--- init.sql
+++ init.sql
@@ -1,4 +1,15 @@
 create database if not exists RentHouse;
 use RentHouse;
+
+-- 创建 area 表
+CREATE TABLE IF NOT EXISTS area (
+    name VARCHAR(255) NOT NULL
+);
+
+-- 创建 facility 表
+CREATE TABLE IF NOT EXISTS facility (
+    name VARCHAR(255) NOT NULL
+);
+
 insert into area(`name`) values ('上城区'),('拱墅区'),('西湖区'),('萧山区'),('滨江区'),('余杭区');
 insert into facility(`name`) values ('无线网络'),('空调'),('暖气'),('热水器'),('免费停车'),('厨房'),('洗衣机'),('电梯'),('允许吸烟'),('允许带宠物'),('允许带儿童'),('允许举办活动'),('允许长期居住'),('允许短期居住'),('允许商业拍摄');
